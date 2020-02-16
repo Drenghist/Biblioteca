545b850a7a40bb268599a3941793feb92730c171
Alex <Alex@Rojo1>
2020-02-16 12:04:33 +0100

    V2

--- src/biblioteca/Socio.java
+++ src/biblioteca/Socio.java
@@ -128,4 +128,39 @@ public class Socio {
     public void setActive(boolean b) {
         this.active=b;
     }
+
+    /**
+     * @param dni the dni to set
+     */
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
